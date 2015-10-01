9370c42a3d206b66b2b9d260d4c582ef793de7de
gwilym <gwilym@gwilymh.com>
2015-10-02 09:43:59 +1000

    babel cache path

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // process.env.BABEL_DISABLE_CACHE = true
-process.env.BABEL_CACHE_PATH = process.env.OPENSHIFT_DATA_DIR
+path = require('path')
+process.env.BABEL_CACHE_PATH = path.join(process.env.OPENSHIFT_DATA_DIR, 'babel-cache.json')
 // Tell babel to include FounderLab packages (starting with fl-)
 require('babel/register')({ignore: /node_modules\/(?!fl-)/})
 require('./server/app')
