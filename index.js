53d3ff1daf64fea55948346a88cb2ed8ec102e46
gwilym <gwilym@gwilymh.com>
2015-10-01 16:57:36 +1000

    test disable babel cache

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+process.env.BABEL_DISABLE_CACHE = true
 // Tell babel to include FounderLab packages (starting with fl-)
 require('babel/register')({ignore: /node_modules\/(?!fl-)/})
 require('./server/app')
