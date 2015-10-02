6543d5d1e38ec9fe744967abd083df844cb7f285
gwilym <gwilym@gwilymh.com>
2015-10-02 10:24:36 +1000

    fix

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+// Fix for babel trying to make a temp file in the home directory where it can't write on openshift
 // process.env.BABEL_DISABLE_CACHE = true
-path = require('path')
-process.env.BABEL_CACHE_PATH = path.join(process.env.OPENSHIFT_DATA_DIR, 'babel-cache.json')
+if (process.env.OPENSHIFT_DATA_DIR) {
+  path = require('path')
+  process.env.BABEL_CACHE_PATH = path.join(process.env.OPENSHIFT_DATA_DIR, 'babel-cache.json')
+}
+
 // Tell babel to include FounderLab packages (starting with fl-)
 require('babel/register')({ignore: /node_modules\/(?!fl-)/})
 require('./server/app')
