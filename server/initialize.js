8a60272eb8d792be0ef5d801feff398587048548
gwilym <gwilym@gwilymh.com>
2015-10-02 14:49:39 +1000

    mongo

--- server/initialize.js
+++ server/initialize.js
@@ -3,7 +3,9 @@ if (!process.env.NODE_ENV) process.env.NODE_ENV = 'development'
 
 if (!process.env.DATABASE_URL) {
   console.log('No process.env.DATABASE_URL present, using openshift values')
-  const base_db_url = `mongodb://${process.env.OPENSHIFT_MONGODB_DB_HOST}:${process.env.OPENSHIFT_MONGODB_DB_PORT}`
+
+  const base_db_url = `mongodb://${OPENSHIFT_MONGO_DB_USERNAME}:${OPENSHIFT_MONGO_DB_PASSWORD}@${process.env.OPENSHIFT_MONGODB_DB_HOST}:${process.env.OPENSHIFT_MONGODB_DB_PORT}`
   process.env.DATABASE_URL = `${base_db_url}}/FounderLab_replaceme`
+
   console.log('process.env.DATABASE_URL set to', process.env.DATABASE_URL)
 }
