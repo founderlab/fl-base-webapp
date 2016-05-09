53aa82129176132ec04ff07019034cacf785a50d
gwilym <gwilym@gwilymh.com>
2016-05-09 11:59:40 +1000

    package

--- server/api/controllers/Profiles.js
+++ server/api/controllers/Profiles.js
@@ -1,24 +1,40 @@
 import _ from 'lodash' // eslint-disable-line
 import RestController from 'backbone-rest'
+import {JSONUtils} from 'backbone-orm'
 import {createAuthMiddleware} from 'fl-auth-server'
+import Profile from '../../models/Profile'
 
-function canAccess(options, callback) {
+export function canAccess(options, callback) {
   const {user, req} = options
-  if (req.method === 'GET') return callback(null, true)
   if (!user) return callback(null, false)
   if (user.admin || user.get('admin')) return callback(null, true)
+
+  const query = JSONUtils.parseQuery(req.query)
+  if (query.$include) return callback(null, false, 'No $include')
+
+  // Allow access for the owner of the profile
+  if (req.method === 'GET' && query.user_id === user.id) {
+    return callback(null, true)
+  }
+
+  // Allow editing for the owner of the profile
+  if (req.method === 'PUT') {
+    return Profile.exists({id: req.params.id, user_id: user.id}, callback)
+  }
+
   callback(null, false)
 }
 
 export default class ProfilesController extends RestController {
   constructor(options) {
     super(options.app, _.defaults({
-      model_type: require('../../models/Profile'),
+      model_type: Profile,
       route: '/api/profiles',
       auth: [...options.auth, createAuthMiddleware({canAccess})],
-      whitelist: {
-
+      templates: {
+        detail: require('../templates/profiles/detail'),
       },
+      default_template: 'detail',
     }, options))
   }
 }
